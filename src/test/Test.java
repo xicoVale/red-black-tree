a08c66e5b50c4fa414540efc2d59e7dfa352220f
xicoVale <xico.vale@hotmail.com>
2016-06-30 16:04:10 +0100

    Added testing options and help

--- src/test/Test.java
+++ src/test/Test.java
@@ -5,40 +5,96 @@ import java.util.Scanner;
 import tree.RBTree;
 
 public class Test {
-	private static Scanner in = new Scanner(System.in);
+	private final static Scanner IN = new Scanner(System.in);
 	private static RBTree tree;
 	
 	public static void main(String[] args) {
-		int [] input = readInput();
+		System.out.print("$ Scale of test? [10 or 10000]: ");
 		
-		populateTree(input);
+		int scale = IN.nextInt();
+		System.out.print("$ In reverse order? [y/n]: ");
 		
-		tree.print();
-		
-		tree.max();
-		
-		tree.min();
-		
-		System.out.println(tree.contains(5));
-		System.out.println(tree.contains(6));
-	}
-	
-	/**
-	 * 
-	 * Process input. The first value should be the number of nodes in the tree.
-	 * This should be followed by the values that will be in the tree.
-	 * 
-	 * @return And int array with the input values.
-	 */
-	private static int [] readInput(){
-		int length = in.nextInt();
-		
-		int [] input = new int [length];
-		
-		for (int i = 0; i < length; i++){
-			input[i] = in.nextInt();
+		String rev = IN.next();
+		boolean reverse = false;
+		if (rev.equals("y") || rev.equals("yes")) {
+			reverse = true;
 		}
 		
+		int [] input;
+		if (scale == 10){
+			input = smallScale(reverse);
+		}
+		else {
+			input = largeScale(reverse);
+		}
+		System.out.print("Seeding tree... ");
+		populateTree(input);
+		System.out.println(" Done.");
+		
+		while(true) {
+			System.out.print("$");
+			String command = IN.next();
+			
+			if (command.equals("help")) {
+				help();
+			}
+			else if (command.equals("print")) {
+				tree.print();
+			}
+			else if (command.equals("insert")) {
+				int value = IN.nextInt();
+				tree.insert(value);
+			}
+			else if (command.equals("delete")) {
+				int value = IN.nextInt();
+				tree.delete(value);
+			}
+			else if (command.equals("contains")) {
+				int value = IN.nextInt();
+				tree.contains(value);
+			}
+			else if (command.equals("find")) {
+				int value = IN.nextInt();
+				tree.find(value);
+			}
+			else if (command.equals("min")) {
+				tree.min();
+			}
+			else if (command.equals("max")) {
+				tree.max();
+			}
+		}
+		
+		
+	}
+	
+	private static int [] smallScale(boolean reverse) {
+		int [] input = new int [10];
+		if (reverse) {
+			for (int i = 10; i > 0; i--) {
+				input[i] = i;
+			}
+		}
+		else {
+			for (int i = 0; i < 10; i++) {
+				input[i] = i + 1;
+			}
+		}
+		return input;
+	}
+	
+	private static int [] largeScale(boolean reverse) {
+		int [] input = new int [10000];
+		if (reverse) {
+			for (int i = 10000; i > 0; i--) {
+				input[i] = i;
+			}
+		}
+		else {
+			for (int i = 0; i < 10000; i++) {
+				input[i] = i + 1;
+			}
+		}
 		return input;
 	}
 	
@@ -50,14 +106,24 @@ public class Test {
 	private static void populateTree(int[] input){
 		for(int i = 0; i < input.length; i++) {
 			if(i == 0) {
-				System.out.println(String.format("Inserting value %s as root... ", input[i]));
 				tree = new RBTree(input[i]);
 			}
 			else {
-				System.out.println(String.format("Inserting value %s... ", input[i]));
 				tree.insert(input[i]);
 			}
 		}
+	}
+	
+	private static void help() {
+		System.out.println("help - Prints list of commands.");
+		System.out.println("print- Prints values in tree.");
+		System.out.println("insert 'x' - Inserts value x into the tree.");
+		System.out.println("delete 'x' - Deletes value x from the tree.");
+		System.out.println("contains 'x' - True if value x is in the tree or false otherwise.");
+		System.out.println("find 'x' - If x exists in the tree print it's value, it's parent and it's children if they exist.");
+		System.out.println("min - Finds the minimum value in the tree");
+		System.out.println("max - Finds the maximum value in the tree");
+		System.out.println("stats 'c' - print costs of running command X");
 	}
 
 }
